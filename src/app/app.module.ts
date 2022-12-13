c81cfbd15b60de9afe1df64f549e2237a28acc3d
halfpast-learn <28054220+halfpast-learn@users.noreply.github.com>
2022-12-13 23:44:50 +0300

    fix app imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import { MainViewComponent } from './main-view/main-view.component';
 import { ProductFeedComponent } from './product-feed/product-feed.component';
 import { FilterComponent } from './filter/filter.component';
 import { HeaderComponent } from './header/header.component';
-import { MatSelectModule } from '@angular/material/select';
-import { MatTableModule } from '@angular/material/table';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { FooterComponent } from './footer/footer.component';
 import { CartComponent } from './cart/cart.component';
@@ -30,9 +33,12 @@ import { ProfileComponent } from './profile/profile.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+
     NoopAnimationsModule,
     MatSelectModule,
-    MatTableModule
+    MatTableModule,
+
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
