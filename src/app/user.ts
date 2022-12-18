36bd591762b3b217a24b4ae537e6579c1f5c7fb9
halfpast-learn <28054220+halfpast-learn@users.noreply.github.com>
2022-12-18 20:21:09 +0300

    add user class

---
+++ src/app/user.ts
@@ -0,0 +1,9 @@
+export class User {
+    user_id: number = 0;
+    username: string = "";
+    password: string = "";
+    first_name: string = "";
+    last_name: string = "";
+    phone: string = "";
+    email: string = "";
+}
