7ccce89025007f9af1b88e10d8d5d1f8486496ab
halfpast-learn <28054220+halfpast-learn@users.noreply.github.com>
2023-02-05 22:00:58 +0300

    add role to user

--- src/app/entities/user.ts
+++ src/app/entities/user.ts
@@ -1,9 +1,10 @@
 export class User {
-    user_id: number = 0;
-    username: string = "";
-    password: string = "";
-    first_name: string = "";
-    last_name: string = "";
-    phone: string = "";
-    email: string = "";
+  user_id: number = 0;
+  username: string = '';
+  password: string = '';
+  first_name: string = '';
+  last_name: string = '';
+  phone: string = '';
+  email: string = '';
+  role: number = 0;
 }
