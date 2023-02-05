ad277eabe0e62f0e4b4002c7de5efd2fca0d115f
halfpast-learn <28054220+halfpast-learn@users.noreply.github.com>
2023-02-05 21:59:58 +0300

    replace auth service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { User } from '../entities/user';
+import { ApiService } from './api.api-service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private apiService: ApiService) {}
+  loggedIn = new BehaviorSubject<boolean>(false);
+
+  currentUser: User = new User();
+
+  ngOnInit() {
+    this.loggedIn.next(false);
+  }
+
+  login(login: string, password: string): void {
+    this.apiService.readUserByName(login).subscribe((result) => {
+      this.currentUser = result;
+      if (result != null && password == result.password) {
+        this.loggedIn.next(true);
+      }
+    });
+  }
+  logout(): void {
+    this.loggedIn.next(false);
+    this.currentUser = new User();
+  }
+  register(login: string, password: string, email: string): void {
+    this.apiService.readUserByName(login).subscribe((result) => {
+      this.currentUser = result;
+      this.loggedIn.next(true);
+    });
+  }
+  getCurrentUser(): User {
+    return this.currentUser;
+  }
+}
