bf01eb3cb8dd987345823542377e985afa330e32
halfpast-learn <28054220+halfpast-learn@users.noreply.github.com>
2023-02-19 04:39:25 +0300

    add cart adding function

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Item } from '../entities/item';
+import { CartService } from '../services/cart-service.service';
 
 @Component({
   selector: 'app-product-card',
@@ -7,11 +8,14 @@ import { Item } from '../entities/item';
   styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent implements OnInit {
-  constructor() {}
-  @Input() itemData: Item = {
+  constructor(private cartService: CartService) {}
+  @Input() item: Item = {
     item_id: -1,
     price: 0,
     description: 'empty',
   };
   ngOnInit(): void {}
+  addToCart(item: Item): void {
+    this.cartService.addItem(item);
+  }
 }
