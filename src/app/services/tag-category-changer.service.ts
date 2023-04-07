4592d621a13ea59c45e77d37550401a0cf027e96
halfpast-learn <28054220+halfpast-learn@users.noreply.github.com>
2023-04-07 21:44:45 +0300

    refactor tag service

--- src/app/services/tag-category-changer.service.ts
+++ src/app/services/tag-category-changer.service.ts
@@ -1,13 +1,11 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Tag } from '../entities/tag';
-import { ApiService } from './api.api-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TagLoaderService {
-  constructor(private apiservice: ApiService) {}
 
   public tagCategory: BehaviorSubject<string> = new BehaviorSubject<string>("Recommended");
   public currentTags: BehaviorSubject<Tag[]> = new BehaviorSubject<Tag[]>([]);
