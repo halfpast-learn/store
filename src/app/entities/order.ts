c72ff49a8162a5cd4f62ee4a2b7ba0dd9f768996
halfpast-learn <28054220+halfpast-learn@users.noreply.github.com>
2023-04-17 03:38:31 +0300

    add new fields to order

--- src/app/entities/order.ts
+++ src/app/entities/order.ts
@@ -3,6 +3,8 @@ import { Item } from "./item";
 export class Order {
     order_id: number = 0;
     status: string = "";
+    address: string = "";
+    contact_information: string = "";
     user_owner: number = 0;
     items: Item[] = [];
 }
