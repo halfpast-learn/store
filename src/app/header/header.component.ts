369b63cfcf63840474cfc8a057e92c1c4eb24624
halfpast-learn <28054220+halfpast-learn@users.noreply.github.com>
2023-04-25 07:03:55 +0300

    remove unused elements

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,31 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-import { TagLoaderService } from '../services/tag-category-changer.service';
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
-  host: {
-    '(document:click)': 'hideMenu($event)',
-  },
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  constructor(private tagloader: TagLoaderService) {}
-  visible: boolean = false;
+  constructor() {}
   ngOnInit(): void {}
-
-  changeMenuVisibility() {
-    this.visible = !this.visible;
-  }
-
-  hideMenu(event: any) {
-    if (!event.target.classList.contains('menuicon')) {
-      this.visible = false;
-    }
-  }
-
-  changeTags(event: Event) {
-    this.tagloader.changeTagCategory((event.target as HTMLElement).innerText);
-  }
 }
